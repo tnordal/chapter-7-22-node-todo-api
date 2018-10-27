3ebc8956fadd0e1cc8ca4b5716441fabbb3c2175
Tom Nordal <tnordal@gmail.com>
2018-10-27 08:42:55 +0200

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,23 +1,17 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {
-  ObjectID
-} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 
-var {
-  mongoose
-} = require('./db/mongoose');
-var {
-  Todo
-} = require('./models/todo');
-var {
-  User
-} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
