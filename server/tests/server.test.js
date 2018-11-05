8ca85d42aa3b564c0d66b1a3d4b5d15bb63455a3
Tom Nordal <tnordal@gmail.com>
2018-11-05 19:34:17 +0100

    Add test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -394,3 +394,25 @@ describe('POST /users/login', () => {
   });
 
 });
+
+describe('DELETE /users/me/token', () => {
+
+  it('should remove auth token on logout', (done) => {
+    // var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmRmMTkyOGMwZjI4MTAxMjhkMGQ1Y2EiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTQxMzQ3NjI0fQ.PY7ODqyzdSd2QC7pqdU6xQYpj8fFExKLxG9nyWfPOzQ';
+    var token = users[0].tokens[0].token
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+});
